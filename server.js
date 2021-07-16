b844812df70cbec7b11962200334507d518daad6
Aldo Cortés <aacortes11@gmail.com>
2021-07-16 17:54:37 -0500

    Updated server

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/concurrency-forms-with-angular'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/concurrency-forms-with-angular/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
