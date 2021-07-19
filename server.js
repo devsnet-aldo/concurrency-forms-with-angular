1cfeeba510aa5a66b348de2369a4e5bc8b637bbc
Aldo Cortés <aacortes11@gmail.com>
2021-07-19 08:10:04 -0500

    Updated package name

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-/*
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-*/
-// Start the app by listening on the default Heroku port
+app.use(requireHTTPS);
+app.use(express.static('./dist/Concurrency-Forms-With-Angular/'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/Concurrency-Forms-With-Angular'}),
+);
+
 app.listen(process.env.PORT || 8080);
